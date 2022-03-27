c3234412111f5a7cdb78bb4005b4a50713293da6
jams10 <jamin3710@gmail.com>
2022-03-27 14:47:29 +0900

    02. 윈도우 창 생성 및 윈도우 띄우기

--- Direct2D_Tutorial/Direct2D_Tutorial/main.cpp
+++ Direct2D_Tutorial/Direct2D_Tutorial/main.cpp
@@ -1,11 +1,53 @@
 #include<Windows.h>
 #include <d2d1.h>
 
+LRESULT CALLBACK WindowProc(
+	HWND hWnd,
+	UINT uMsg,
+	WPARAM wParam,
+	LPARAM lParam
+)
+{
+	if (uMsg == WM_DESTROY)
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
 int WINAPI wWinMain(
 	HINSTANCE hInstance,
 	HINSTANCE prevInstance,
-	LPWSTR,
+	LPWSTR cmd,
 	int nCmdShow)
 {
+	WNDCLASSEX windowClass;
+	ZeroMemory(&windowClass, sizeof(WNDCLASSEX));
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	windowClass.lpfnWndProc = WindowProc;
+	windowClass.lpszClassName = L"MainWindow";
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+
+	RegisterClassEx(&windowClass);
+
+	HWND hWnd = CreateWindow(L"MainWindow", L"D2D Tutorial", WS_OVERLAPPEDWINDOW, 100, 100, 800, 600, NULL, NULL, hInstance, 0);
+
+	if (!hWnd)
+	{
+		return -1;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+
+	MSG msg;
+	while (GetMessage(&msg, hWnd, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
 	return 0;
 }
